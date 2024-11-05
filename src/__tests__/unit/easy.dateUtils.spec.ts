dcc6fa9a7ed72ed6141ea068603b9daf02c885d1
정원 <jwha94@gmail.com>
2024-11-06 00:34:06 +0900

    formatDate 대한 테스트코드 작성

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -1,6 +1,7 @@
 import { Event } from '../../types';
 import {
   fillZero,
+  formatDate,
   formatMonth,
   formatWeek,
   getDaysInMonth,
@@ -319,11 +320,19 @@ describe('fillZero', () => {
 });
 
 describe('formatDate', () => {
-  it('날짜를 YYYY-MM-DD 형식으로 포맷팅한다', () => {});
+  it('날짜를 YYYY-MM-DD 형식으로 포맷팅한다', () => {
+    expect(formatDate(new Date('2024-11-6'))).toBe('2024-11-06');
+  });
 
-  it('day 파라미터가 제공되면 해당 일자로 포맷팅한다', () => {});
+  it('day 파라미터가 제공되면 해당 일자로 포맷팅한다', () => {
+    expect(formatDate(new Date('2024-11-6'), 12)).toBe('2024-11-12');
+  });
 
-  it('월이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {});
+  it('월이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {
+    expect(formatDate(new Date('2024-1-16'))).toBe('2024-01-16');
+  });
 
-  it('일이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {});
+  it('일이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {
+    expect(formatDate(new Date('2024-11-6'))).toBe('2024-11-06');
+  });
 });
