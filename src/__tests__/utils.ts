b5753fe9c5fa1e32e1771608f38f0ac02016c933
JayeHa <jwha94@gmail.com>
2024-11-06 10:26:34 +0900

    refactor: generateTestEvent 추가

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,3 +1,4 @@
+import { Event } from '../types';
 import { fillZero } from '../utils/dateUtils';
 
 export const assertDate = (date1: Date, date2: Date) => {
@@ -9,4 +10,42 @@ export const parseHM = (timestamp: number) => {
   const h = fillZero(date.getHours());
   const m = fillZero(date.getMinutes());
   return `${h}:${m}`;
+};
+
+type BaseEvent = Pick<Event, 'id' | 'title' | 'date' | 'startTime' | 'endTime'>;
+type OptionalEventProperties = Partial<
+  Pick<Event, 'description' | 'location' | 'category' | 'repeat' | 'notificationTime'>
+>;
+
+type TestEvent = BaseEvent & OptionalEventProperties;
+
+export const generateTestEvent = ({
+  id,
+  title,
+  date,
+  startTime,
+  endTime,
+
+  description,
+  location,
+  category = '업무',
+  repeat = { type: 'none', interval: 0 },
+  notificationTime = 10,
+}: TestEvent): Event => {
+  return {
+    id,
+    title,
+    date,
+    startTime,
+    endTime,
+    description: description ?? `이벤트 ${id} 입니다.`,
+    location: location ?? `이벤트 ${id}의 장소`,
+    category,
+    repeat,
+    notificationTime,
+  };
+};
+
+export const generateTestEvents = (events: TestEvent[]): Event[] => {
+  return events.map((event) => generateTestEvent(event));
 };
