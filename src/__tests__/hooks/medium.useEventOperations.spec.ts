5f952b9307938d2f08af1705dbc4d8e9310a184c
JayeHa <jwha94@gmail.com>
2024-11-06 17:29:51 +0900

    wip: useEventOperations 테스트

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,14 +1,9 @@
-import { act, renderHook } from '@testing-library/react';
-import { http, HttpResponse } from 'msw';
+import { renderHook, waitFor } from '@testing-library/react';
+import { act } from 'react';
 
-import {
-  setupMockHandlerCreation,
-  setupMockHandlerDeletion,
-  setupMockHandlerUpdating,
-} from '../../__mocks__/handlersUtils.ts';
+import { setupMockHandlerCreation } from '../../__mocks__/handlersUtils.ts';
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
-import { server } from '../../setupTests.ts';
-import { Event } from '../../types.ts';
+import { generateTestEvent, generateTestEvents } from '../utils.ts';
 
 // ? Medium: 아래 toastFn과 mock과 이 fn은 무엇을 해줄까요?
 const toastFn = vi.fn();
@@ -21,9 +16,36 @@ vi.mock('@chakra-ui/react', async () => {
   };
 });
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+  setupMockHandlerCreation(
+    generateTestEvents([{ id: '1', title: '이벤트 1', date: '2024-10-01' }])
+  );
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+  const { result } = renderHook(() => useEventOperations(false));
+
+  await waitFor(() => {
+    expect(result.current.events).toHaveLength(1);
+    expect(result.current.events[0].title).toBe('이벤트 1');
+  });
+});
+
+it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+  setupMockHandlerCreation(
+    generateTestEvents([{ id: '1', title: '이벤트 1', date: '2024-11-01' }])
+  );
+
+  const { result } = renderHook(() => useEventOperations(false));
+
+  act(() => {
+    result.current.saveEvent(generateTestEvent({ id: '1', title: '이벤트 2', date: '2024-11-2' }));
+  });
+
+  await waitFor(() => {
+    expect(result.current.events).toHaveLength(2);
+    expect(result.current.events[0].title).toBe('이벤트 1');
+    expect(result.current.events[1].title).toBe('이벤트 2');
+  });
+});
 
 it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
 
